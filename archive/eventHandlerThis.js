39d04bbdefd7815d369b16631efc98711ac8aa5c
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2026-02-12 08:24:06 -0500

    Update eventHandlerThis.js

--- archive/eventHandlerThis.js
+++ archive/eventHandlerThis.js
@@ -1,1 +1,10 @@
+document.body.innerHTML = '<button id="myButton">Click It!</button>';
+const btn = document.getElementById("myButton");
 
+btn.addEventListener("click", function() {
+    console.log(this); // <button> element
+});
+
+btn.addEventListener("click", () => {
+    console.log(this); // Window (outer scope)
+});
