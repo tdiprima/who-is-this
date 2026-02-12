a35cdf29fea3243549ce0fbbda66882e5cb217c0
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2026-02-12 07:57:55 -0500

    Add global-this-behavior.js to demonstrate 'this' usage
    This file demonstrates the behavior of 'this' in global scope under different modes.

---
+++ global-this-behavior.js
@@ -0,0 +1,7 @@
+// global-this-behavior.js
+// This code demonstrates 'this' in global scope: window in browser non-strict, undefined in strict mode.
+
+console.log(this); // Window object in browser (non-strict)
+
+"use strict";
+console.log(this); // undefined
