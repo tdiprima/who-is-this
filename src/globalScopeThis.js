44f707352995c4318a1a29262185b32f49ce7dc7
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2026-02-12 07:56:49 -0500

    Delete globalScopeThis.js

--- src/globalScopeThis.js
+++
@@ -1,8 +0,0 @@
-/*
-Filename: globalScopeThis.js
-This code shows what 'this' is in global scope, with and without strict mode.
-*/
-console.log("Non-strict mode:", this);  // Window/global object in most environments
-
-"use strict";
-console.log("Strict mode:", this);  // undefined
